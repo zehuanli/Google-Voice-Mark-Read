e3ca4495bea3d9b9d6f4f55e7454acf4563842ff
Zehuan Li <zhlijhu@gmail.com>
2021-06-05 14:22:01 -0400

    init

---
+++ app/src/main/java/com/upbad/apps/vogo/util/HookParams.java
@@ -0,0 +1,13 @@
+package com.upbad.apps.vogo.util;
+
+public class HookParams {
+    public static final String VOICE_PACKAGE_NAME = "com.google.android.apps.googlevoice";
+
+    public static final String VOICE_NOTIFICATION_GENERATOR_CLASS = "gq";
+    public static final String VOICE_NOTIFICATION_ATTRIBUTE_CLASS = "gl";
+    public static final String VOICE_NOTIFICATION_GENERATOR_CLASS_NOTIFICATION_BUILDER_FIELD = "a";
+    public static final String VOICE_NOTIFICATION_ATTRIBUTE_CLASS_CAR_BUNDLE_GETTER = "b";
+
+    // This is from android.app.Notification.Builder
+    public static final String NOTIFICATION_BUILDER_ACTION_LIST = "mActions";
+}
