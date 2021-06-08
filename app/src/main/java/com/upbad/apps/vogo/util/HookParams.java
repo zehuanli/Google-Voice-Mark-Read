265e7cfafbf7c6cccf7f5e2d6b26383f08d1323f
Zehuan Li <22104836+zehuanli@users.noreply.github.com>
2021-06-08 14:36:34 -0400

    Version 2021.05.376253066 (3224871)

--- app/src/main/java/com/upbad/apps/vogo/util/HookParams.java
+++ app/src/main/java/com/upbad/apps/vogo/util/HookParams.java
@@ -3,8 +3,9 @@ package com.upbad.apps.vogo.util;
 public class HookParams {
     public static final String VOICE_PACKAGE_NAME = "com.google.android.apps.googlevoice";
 
-    public static final String VOICE_NOTIFICATION_GENERATOR_CLASS = "gq";
-    public static final String VOICE_NOTIFICATION_ATTRIBUTE_CLASS = "gl";
+    // Version 2021.05.376253066 (3224871)
+    public static final String VOICE_NOTIFICATION_GENERATOR_CLASS = "gr";
+    public static final String VOICE_NOTIFICATION_ATTRIBUTE_CLASS = "gm";
     public static final String VOICE_NOTIFICATION_GENERATOR_CLASS_NOTIFICATION_BUILDER_FIELD = "a";
     public static final String VOICE_NOTIFICATION_ATTRIBUTE_CLASS_CAR_BUNDLE_GETTER = "b";
 
