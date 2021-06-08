de0a5340854b28a3910f55a44796266f8550db2c
Zehuan Li <22104836+zehuanli@users.noreply.github.com>
2021-06-08 17:01:40 -0400

    Improved ways to search objects - this should survive more Google Voice versions until there's a significant change

--- app/src/main/java/com/upbad/apps/vogo/plugin/NotificationActions.java
+++ app/src/main/java/com/upbad/apps/vogo/plugin/NotificationActions.java
@@ -3,6 +3,7 @@ package com.upbad.apps.vogo.plugin;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.os.Bundle;
+import android.os.Parcelable;
 
 import java.util.ArrayList;
 
@@ -12,37 +13,53 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import static com.upbad.apps.vogo.util.HookParams.NOTIFICATION_BUILDER_ACTION_LIST;
-import static com.upbad.apps.vogo.util.HookParams.VOICE_NOTIFICATION_ATTRIBUTE_CLASS_CAR_BUNDLE_GETTER;
-import static com.upbad.apps.vogo.util.HookParams.VOICE_NOTIFICATION_GENERATOR_CLASS;
-import static com.upbad.apps.vogo.util.HookParams.VOICE_NOTIFICATION_ATTRIBUTE_CLASS;
-import static com.upbad.apps.vogo.util.HookParams.VOICE_NOTIFICATION_GENERATOR_CLASS_NOTIFICATION_BUILDER_FIELD;
 import static com.upbad.apps.vogo.util.ReflectionUtil.log;
 
 public class NotificationActions implements IPlugin {
+
+    PendingIntent pendingIntent = null;
+
     @Override
     public void hook(final XC_LoadPackage.LoadPackageParam lpparam, final ClassLoader classLoader) {
         log("NotificationActions hook() started");
         // Reverse engineer tip:
-        //   - search for "notify" for use of `Notification` class
-        //   - search for "addAction" for adding actions (buttons on the pop-up)
+        //   - search "notify" for the use of `Notification` class
+        //   - search "addAction" for adding actions (buttons on the pop-up)
         //   - packages start with `defpackage` should be named without this prefix
-        // XposedBridge.hookAllConstructors may also be used since there's only one constructor in this class
-        XposedBridge.hookMethod(XposedHelpers.findConstructorExact(VOICE_NOTIFICATION_GENERATOR_CLASS, classLoader, VOICE_NOTIFICATION_ATTRIBUTE_CLASS), new XC_MethodHook() {
+        //   - search "setRemoteInputHistory" or "actionIntent" for the notification generator
 
+        // There is a bundle in the notification generator
+        // that contains a hidden PendingIntent (probably used for Android Auto),
+        // which can be directly used to mark the message as read.
+
+        // Hook the unique statement that puts the hidden PendingIntent in a bundle
+        XposedHelpers.findAndHookMethod("android.os.Bundle", classLoader, "putParcelable", String.class, Parcelable.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) {
                 try {
-                    Object notificationBuilder = XposedHelpers.getObjectField(param.thisObject, VOICE_NOTIFICATION_GENERATOR_CLASS_NOTIFICATION_BUILDER_FIELD);
+                    if (param.args[0].equals("actionIntent")) {
+                        pendingIntent = (PendingIntent) param.args[1];
+                    }
+                } catch (Error | Exception e) {
+                    log(e);
+                }
+            }
+        });
+
+        // Hook the unique statement that invokes a function of the Notification.Builder in the notification generator
+        XposedHelpers.findAndHookMethod("android.app.Notification.Builder", classLoader, "setRemoteInputHistory", CharSequence[].class, new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) {
+                try {
+                    Object notificationBuilder = param.thisObject;
                     ArrayList<Notification.Action> actions = (ArrayList<Notification.Action>) XposedHelpers.getObjectField(notificationBuilder, NOTIFICATION_BUILDER_ACTION_LIST);
                     if (actions.size() != 1) {
                         return;
                     }
-                    // There is bundle contained in the first (and only) parameter object.
-                    // This bundle contains a hidden PendingIntent (probably used for Android Auto),
-                    // which can be directly used to mark the message as read.
-                    // This bundle can be obtained using the integrated getter below, or directly via `getObjectField`
-                    Bundle bundle = (Bundle) XposedHelpers.callMethod(param.args[0], VOICE_NOTIFICATION_ATTRIBUTE_CLASS_CAR_BUNDLE_GETTER);
-                    PendingIntent pendingIntent = bundle.getBundle("android.car.EXTENSIONS").getBundle("invisible_actions").getBundle("0").getParcelable("actionIntent");
+
+                    if (pendingIntent == null) {
+                        return;
+                    }
                     Notification.Action.Builder actionBuilder = new Notification.Action.Builder(0, "Mark as Read", pendingIntent);
                     XposedHelpers.callMethod(notificationBuilder, "addAction", actionBuilder.build());
                 } catch (Error | Exception e) {
--- app/src/main/java/com/upbad/apps/vogo/util/HookParams.java
+++ app/src/main/java/com/upbad/apps/vogo/util/HookParams.java
@@ -3,12 +3,6 @@ package com.upbad.apps.vogo.util;
 public class HookParams {
     public static final String VOICE_PACKAGE_NAME = "com.google.android.apps.googlevoice";
 
-    // Version 2021.05.376253066 (3224871)
-    public static final String VOICE_NOTIFICATION_GENERATOR_CLASS = "gr";
-    public static final String VOICE_NOTIFICATION_ATTRIBUTE_CLASS = "gm";
-    public static final String VOICE_NOTIFICATION_GENERATOR_CLASS_NOTIFICATION_BUILDER_FIELD = "a";
-    public static final String VOICE_NOTIFICATION_ATTRIBUTE_CLASS_CAR_BUNDLE_GETTER = "b";
-
     // This is from android.app.Notification.Builder
     public static final String NOTIFICATION_BUILDER_ACTION_LIST = "mActions";
 }
